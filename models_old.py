a19b53b607f751c51a9ccad0ef8e7b1b4f2b1eaa
morrillo <gustavo.orrillo@moldeo.coop>
2016-12-02 19:28:47 -0300

    Added cupon and tarjeta to order view

--- models_old.py
+++ models_old.py
@@ -10,7 +10,7 @@ from openerp.tools.translate import _
 
 import openerp.addons.decimal_precision as dp
 import openerp.addons.product.product
-
+import math
 
 class pos_make_payment(osv.osv_memory):
 	_inherit = 'pos.make.payment'
@@ -27,6 +27,7 @@ class pos_make_payment(osv.osv_memory):
 	        order = order_obj.browse(cr, uid, active_id, context=context)
 	        amount = order.amount_total - order.amount_paid
         	data = self.read(cr, uid, ids, context=context)[0]
+		cuotas = None
 		if data['cuotas_id']:
 			cuotas = self.pool.get('sale.cuotas').browse(cr,uid,data['cuotas_id'][0])
 			if cuotas:
@@ -46,5 +47,18 @@ class pos_make_payment(osv.osv_memory):
 					data['amount'] = amount * (1+cuotas.coeficiente)
 					return_id = self.pool.get('pos.make.payment').write(cr,uid,ids,vals)
 		res = super(pos_make_payment,self).check(cr,uid,ids,context)
+		if cuotas:
+			total_amount = amount * ( 1 + cuotas.coeficiente )
+			statement_id = self.pool.get('account.bank.statement.line').\
+				search(cr,uid,[('pos_statement_id','=',context['active_id']),\
+					('journal_id','=',data['journal_id'][0])],order='id desc',limit=1)
+			if statement_id:
+				statement = self.pool.get('account.bank.statement.line').browse(cr,uid,statement_id)
+				if math.floor(statement.amount) == math.floor(total_amount):
+					vals = {
+						'nro_cupon': data.get('nro_cupon','N/A'),				
+						'nro_tarjeta': data.get('nro_tarjeta','N/A'),				
+						}
+					return_id = self.pool.get('account.bank.statement.line').write(cr,uid,statement_id,vals)
 		return {'type': 'ir.actions.act_window_close'}
 
