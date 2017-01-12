64d26f4f07a7081f8aa76311204a9de4cdcb2b86
morrillo <gustavo.orrillo@moldeo.coop>
2017-01-12 11:40:23 -0300

    Added reports

--- __openerp__.py
+++ __openerp__.py
@@ -8,6 +8,10 @@
 	#'security/security.xml',
 	'wizard/wizard_view.xml',
 	'sale_view.xml',
+	'report/pos_order_details.xml',
+	'report/pos_session_detail.xml',
+	'report/pos_session_resume.xml',
+	'report/pos_small_report.xml'
     ],
     'demo': [
     ],
